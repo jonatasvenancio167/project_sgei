992ed5dfac62e0de2b9ec5e15c9d15c031f5769e
Jonatas Venancio <jvenancio167@hotmail.com>
2026-05-11 21:30:19 -0300

    feat: integrate Devise for user authentication and add flash message support to application layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   enum status: { active: 0, inactive: 1 }
   enum role: { admin: 0, leader: 1, member: 2 }
