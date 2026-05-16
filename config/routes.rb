e6b8da4aeda5d1816f22ed3b0123a6abbebff7c1
Jonatas Venancio <jvenancio167@hotmail.com>
2026-05-16 01:04:06 -0300

    feat: implement application sidebar and add comprehensive initial database seeds

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  get "overview", to: "overview#index", as: :overview
+  
   # Defines the root path route ("/")
-  root "users#index"
+  root "overview#index"
 end
