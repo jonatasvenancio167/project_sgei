32b1ee365c0f47a67fb4360caf4a89d3796bef78
Jonatas Venancio <jvenancio167@hotmail.com>
2026-07-10 16:46:17 -0300

    adicionando a logica para os usuarios nao autorizados

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+  include Pagy::Backend
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
-  include Pagy::Backend
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  private
+
+  def user_not_authorized
+    respond_to do |format|
+      format.html { redirect_to root_path, alert: "Você não tem permissão para acessar esta página." }
+      format.json { render json: { error: "não autorizado" }, status: :forbidden }
+      format.any  { head :forbidden }
+    end
+  end
 end
